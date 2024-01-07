6ffe37b872b1899dcd874873630fb72fe68a68f2
Nishant Bansal <47652694+Nishant-Bansal@users.noreply.github.com>
2024-01-07 14:55:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,32 +4,22 @@
  * @return {number}
  */
 var maxAbsValExpr = function(arr1, arr2) {
-    let max1 = Number.MIN_VALUE;
-    let max2 = Number.MIN_VALUE;
-    let max3 = Number.MIN_VALUE;
-    let max4 = Number.MIN_VALUE;
-    let min1 = Number.MAX_VALUE;
-    let min2 = Number.MAX_VALUE;
-    let min3 = Number.MAX_VALUE;
-    let min4 = Number.MAX_VALUE;
+    let a1 = [], a2 = [], a3 = [], a4 = [], resultArr = [];
 
-    let len = arr1.length
-
-    for(let i=0; i < len; i++){
-        max1 = Math.max(arr1[i] + arr2[i] + i, max1)
-        min1 = Math.min(arr1[i] + arr2[i] + i, min1)
-
-        max2 = Math.max(arr1[i] - arr2[i] + i, max2)
-        min2 = Math.min(arr1[i] - arr2[i] + i, min2)
-
-        max3 = Math.max(-arr1[i] + arr2[i] + i, max3)
-        min3 = Math.min(-arr1[i] + arr2[i] + i, min3)
-
-        max4 = Math.max(-arr1[i] - arr2[i] + i, max4)
-        min4 = Math.min(-arr1[i] - arr2[i] + i, min4) 
+    for(let i = 0; i < arr1.length; i++){
+        a1.push(arr1[i]+arr2[i]+i)
+        a2.push(-arr1[i]+arr2[i]+i)
+        a3.push(arr1[i]-arr2[i]+i)
+        a4.push(-arr1[i]-arr2[i]+i)
     }
 
-    return Math.max(Math.max(max1-min1, max2-min2), Math.max(max3-min3, max4-min4));
+    resultArr.push(Math.max(...a1)-Math.min(...a1))
+    resultArr.push(Math.max(...a2)-Math.min(...a2))
+    resultArr.push(Math.max(...a3)-Math.min(...a3))
+    resultArr.push(Math.max(...a4)-Math.min(...a4))
+
+    return Math.max(...resultArr)
+
 };
 
-console.log("maxAbsValExpr ==> ", maxAbsValExpr([1,2,3,4], [-1,4,5,6]))
+console.log("maxAbsValExpr ==> ", maxAbsValExpr([1,-2], [8, 8]))
